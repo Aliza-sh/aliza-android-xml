b007caef6031b753deed3d4f6110b25e233ce5ad
Aliza <alizashahsavari@gmail.com>
2024-03-30 23:00:50 +0330

    initial commit

---
+++ app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.aliza.alizaandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
