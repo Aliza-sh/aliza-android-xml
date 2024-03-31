2ae36da8d0cae212def9e0cda9fc65bb39b16fca
Aliza <alizashahsavari@gmail.com>
2024-03-31 17:46:56 +0330

    scale animation added

--- app/src/main/java/com/aliza/alizaandroid/FragmentViewAnimations.kt
+++ app/src/main/java/com/aliza/alizaandroid/FragmentViewAnimations.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import androidx.fragment.app.Fragment
 import com.aliza.alizaandroid.databinding.FragmentViewAnimationsBinding
 
@@ -27,7 +28,23 @@ class FragmentViewAnimations : Fragment() {
             alphaAnimation()
         }
 
+        binding.radiogroupScale.setOnCheckedChangeListener { _, i ->
 
+            when (i) {
+                R.id.radio_pivot -> {
+                    binding.btnAnimScale.setOnClickListener {
+                        scaleAnimation(true)
+                    }
+                }
+
+                R.id.radio_but_pivot -> {
+                    binding.btnAnimScale.setOnClickListener {
+                        scaleAnimation(false)
+                    }
+                }
+
+            }
+        }
     }
 
     private fun alphaAnimation() {
@@ -43,5 +60,42 @@ class FragmentViewAnimations : Fragment() {
             override fun onAnimationRepeat(animation: Animation?) {}
         })
         binding.imgAnimAlpha.startAnimation(alphaAnimation)
+    }
+
+    lateinit var scaleAnimation: ScaleAnimation
+    private fun scaleAnimation(pivot: Boolean) {
+        if (pivot) {
+            scaleAnimation = ScaleAnimation(
+                0f, 1f,
+                0f, 1f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+            )
+            scaleAnimation.duration = 2000
+            scaleAnimation.fillAfter = true
+            scaleAnimation.repeatCount = 5
+            scaleAnimation.repeatMode = Animation.REVERSE
+        } else {
+            scaleAnimation = ScaleAnimation(
+                0f, 1f,
+                0f, 1f
+            )
+            scaleAnimation.duration = 2000
+            scaleAnimation.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {}
+                override fun onAnimationEnd(animation: Animation?) {
+                    scaleAnimation = ScaleAnimation(
+                        1f, -0f,
+                        1f, 1f
+                    )
+                    scaleAnimation.duration = 2000
+                    scaleAnimation.fillAfter = true
+                    binding.imgAnimScale.startAnimation(scaleAnimation)
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {}
+            })
+        }
+        binding.imgAnimScale.startAnimation(scaleAnimation)
     }
 }
