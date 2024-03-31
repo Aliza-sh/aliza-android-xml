3b6a24fe98a08a3ac424302c4056dc21c6e3b8b6
Aliza <alizashahsavari@gmail.com>
2024-03-31 16:24:33 +0330

    alpha animation added

--- app/src/main/java/com/aliza/alizaandroid/FragmentViewAnimations.kt
+++ app/src/main/java/com/aliza/alizaandroid/FragmentViewAnimations.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.fragment.app.Fragment
 import com.aliza.alizaandroid.databinding.FragmentViewAnimationsBinding
 
@@ -15,13 +17,31 @@ class FragmentViewAnimations : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentViewAnimationsBinding.inflate(layoutInflater,container,false)
+        binding = FragmentViewAnimationsBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        binding.btnAnimAlpha.setOnClickListener {
+            alphaAnimation()
+        }
 
 
+    }
+
+    private fun alphaAnimation() {
+
+        val alphaAnimation = AlphaAnimation(1f, 0f)
+        alphaAnimation.duration = 1000
+        alphaAnimation.fillAfter = true
+        alphaAnimation.repeatCount = 10
+        alphaAnimation.repeatMode = Animation.REVERSE
+        alphaAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+        })
+        binding.imgAnimAlpha.startAnimation(alphaAnimation)
     }
 }
