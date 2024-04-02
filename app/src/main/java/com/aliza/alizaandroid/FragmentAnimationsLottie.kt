a836262f910170067a58d79086ea15bba12f45f3
Aliza <alizashahsavari@gmail.com>
2024-04-02 04:32:23 +0330

    lottie animation added

--- app/src/main/java/com/aliza/alizaandroid/FragmentAnimationsLottie.kt
+++ app/src/main/java/com/aliza/alizaandroid/FragmentAnimationsLottie.kt
@@ -1,9 +1,11 @@
 package com.aliza.alizaandroid
 
+import android.animation.Animator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
 import androidx.fragment.app.Fragment
 import com.aliza.alizaandroid.databinding.FragmentAnimationsLottieBinding
 
@@ -17,5 +19,13 @@ class FragmentAnimationsLottie : Fragment() {
     ): View? {
         binding = FragmentAnimationsLottieBinding.inflate(layoutInflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.btnAnim.setOnClickListener {
+            binding.animLottie.playAnimation()
+        }
+
     }
 }
