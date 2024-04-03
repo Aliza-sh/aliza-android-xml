384b27b4e3b8d8c177179a7c79281fba65affa1a
Aliza <alizashahsavari@gmail.com>
2024-04-04 00:51:29 +0330

    Add method to get caller class name in BaseFragment.

--- app/src/main/java/com/aliza/alizaandroid/base/BaseFragment.kt
+++ app/src/main/java/com/aliza/alizaandroid/base/BaseFragment.kt
@@ -42,4 +42,25 @@ abstract class BaseFragment<VB : ViewBinding>(
             AnimationUtils.loadAnimation(context, R.anim.anim_slide_out_right)
         }
     }
+
+    //Method to get the name of the caller's class.
+    protected open fun getCallerClassName(): String {
+        val stackTrace = Thread.currentThread().stackTrace
+        var callerClassName = ""
+        for (stackTraceElement in stackTrace) {
+            if (stackTraceElement.className != this.javaClass.name) {
+                callerClassName = stackTraceElement.className
+                break
+            }
+        }
+        return callerClassName
+    }
+    //Implement the following method in the caller class to return the name.
+    /*
+    override fun getCallerClassName(): String {
+        return "CallerActivity"
+
+    }
+    */
+
 }
