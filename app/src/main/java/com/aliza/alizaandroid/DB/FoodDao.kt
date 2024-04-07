1bfd967fd6377566628e2b215513bc1acb581de0
Aliza <alizashahsavari@gmail.com>
2024-04-07 21:45:48 +0330

    BaseDao implementation for repetitive operations

--- app/src/main/java/com/aliza/alizaandroid/DB/FoodDao.kt
+++ app/src/main/java/com/aliza/alizaandroid/DB/FoodDao.kt
@@ -1,32 +1,24 @@
 package com.aliza.alizaandroid.DB
 
 import androidx.room.*
+import com.aliza.alizaandroid.base.BaseDao
 
 @Dao
-interface FoodDao {
+interface FoodDao :BaseDao<Food>{
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertOrUpdateFood(food: Food)
-
-//    @Insert
-//    fun insertFood(food: Food)
+    /*@Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertOrUpdateFood(food: Food)*/
 
     @Insert
     fun insertDefaultFood(foodList:List<Food>)
 
-    @Delete
-    fun deleteFood(food: Food)
-
-//    @Update
-//    fun updateFood(food: Food)
-
     @Query("SELECT * FROM tableFood WHERE txtSubject LIKE '%' || :searching || '%' ")
-    fun searchFood(searching: String): List<Food>
+    fun search(searching: String): List<Food>
 
     @Query("SELECT * FROM tableFood")
-    fun getAllFood(): List<Food>
+    fun getAll(): List<Food>
 
     @Query("DELETE FROM tableFood")
-    fun deleteAllFood()
+    fun deleteAll()
 }
 
