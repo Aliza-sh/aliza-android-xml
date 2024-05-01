72585ff00306f2c89c3e685eaa2227b8f56b09c2
Aliza <alizashahsavari@gmail.com>
2024-05-01 22:35:19 +0330

    Design and initial implementation of Ui app.

--- app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
+++ app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
@@ -1,14 +1,31 @@
 package com.aliza.alizaandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.aliza.alizaandroid.base.BaseActivity
 import com.aliza.alizaandroid.databinding.ActivityMainBinding
+import com.aliza.alizaandroid.ui.FragmentVideoView
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun inflateBinding() = ActivityMainBinding.inflate(layoutInflater)
 
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding.btnFrgVideoView.setOnClickListener {
+            transactionFragment(FragmentVideoView())
+        }
+
     }
+    private fun transactionFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction
+            .add(R.id.frame_container, fragment, null)
+            .addToBackStack(null)
+            .commit()
+    }
+
 }
