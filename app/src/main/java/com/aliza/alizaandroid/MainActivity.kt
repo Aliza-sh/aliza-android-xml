98f648218abf248acb736c1f08ded3841d47f5a6
Aliza <alizashahsavari@gmail.com>
2024-05-09 02:31:59 +0330

    Implement a ForegroundService

--- app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
+++ app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.aliza.alizaandroid
 
+import android.Manifest.permission.POST_NOTIFICATIONS
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.aliza.alizaandroid.base.BaseActivity
 import com.aliza.alizaandroid.databinding.ActivityMainBinding
 
@@ -13,11 +18,38 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
         startBackgroundService()
 
+        requestNotificationPermission {
+            startForegroundService()
+        }
+
     }
 
     private fun startBackgroundService() {
         val intent = Intent(this , BackgroundService::class.java)
         startService(intent)
     }
+    private fun startForegroundService() {
+        val intent = Intent(this , ForegroundService::class.java)
+        startService(intent)
+    }
+
+    private fun requestNotificationPermission(onPermissionGranted: () -> Unit) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(POST_NOTIFICATIONS),
+                    1
+                )
+            } else {
+                onPermissionGranted()
+            }
+        } else {
+            //Handle notification permission request for pre-Android 13 versions (if needed)
+        }
+    }
+
 
 }
