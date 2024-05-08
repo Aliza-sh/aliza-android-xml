dfdb59d7a4af52c175dfb990989105f5f6e561a9
Aliza <alizashahsavari@gmail.com>
2024-05-08 04:43:20 +0330

    Activity implementation 1. Activity as a container 2. Addressing starting point 3. Save recycler display mode

--- app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
+++ app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.aliza.alizaandroid
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.aliza.alizaandroid.base.BaseActivity
 import com.aliza.alizaandroid.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
-    override fun inflateBinding() = ActivityMainBinding.inflate(layoutInflater)
+// linear => 0
+// grid => 1
 
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    companion object {
+        var ourViewType = 0
+        var ourSpanCount = 1
+    }
+
+    override fun inflateBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val file = getExternalFilesDir(null)!!
+        val path = file.path
+
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.frame_main_container , FileFragment(path))
+        transaction.commit()
+
     }
+
 }
