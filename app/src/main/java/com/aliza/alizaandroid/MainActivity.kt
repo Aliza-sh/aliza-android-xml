3d73c60a0280abd0098d96d38374b132177e5b50
Aliza <alizashahsavari@gmail.com>
2024-05-09 18:17:32 +0330

    Implement a chainWorkers

--- app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
+++ app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
@@ -8,13 +8,18 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.OverwritingInputMerger
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import androidx.work.workDataOf
 import com.aliza.alizaandroid.base.BaseActivity
+import com.aliza.alizaandroid.chainWorkers.WorkerDownLoadImag
+import com.aliza.alizaandroid.chainWorkers.WorkerEditImag
+import com.aliza.alizaandroid.chainWorkers.WorkerSaveImag
 import com.aliza.alizaandroid.databinding.ActivityMainBinding
 import java.util.concurrent.TimeUnit
 
@@ -39,7 +44,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 //************************************************************
 
         // 3. work manager should do works =>
-        workManager.enqueue(sampleOneTimeWorker)
+        //workManager.enqueue(sampleOneTimeWorker)
 
         workManager.enqueue(sendAndReceiveDataOneTimeWorker)
         workManager
@@ -59,6 +64,33 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             periodicWorker()
         }
 //************************************************************
+        /*workManager
+            .beginWith(OneTimeWorkRequest.from(WorkerDownLoadImag::class.java))
+            .then(OneTimeWorkRequest.from(WorkerEditImag::class.java))
+            .then(OneTimeWorkRequest.from(WorkerSaveImag::class.java))
+            .enqueue()*/
+
+        val downLoadImagRequest = OneTimeWorkRequest.Builder(WorkerDownLoadImag::class.java)
+            .setInputData(
+                workDataOf(
+                    "urlImage" to "https://www.google.com",
+                )
+            )
+            .build()
+
+        val editImagWorkRequest = OneTimeWorkRequest.Builder(WorkerEditImag::class.java)
+            .setInputMerger(OverwritingInputMerger::class.java)
+            .build()
+
+        val saveImagWorkRequest = OneTimeWorkRequest.Builder(WorkerSaveImag::class.java)
+            .setInputMerger(OverwritingInputMerger::class.java)
+            .build()
+
+        workManager
+            .beginUniqueWork("chainWorkers",ExistingWorkPolicy.KEEP,downLoadImagRequest)
+            .then(editImagWorkRequest)
+            .then(saveImagWorkRequest)
+            .enqueue()
 
     }
 
---
+++ app/src/main/java/com/aliza/alizaandroid/chainWorkers/WorkerSaveImag.kt
@@ -0,0 +1,48 @@
+package com.aliza.alizaandroid.chainWorkers
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import java.lang.Exception
+
+class WorkerSaveImag(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+
+    override fun doWork(): Result {
+
+        return try {
+
+            val result = doMyJob()
+            Result.success(
+                workDataOf(
+                    "imageSave" to result
+                )
+            )
+
+        } catch (ex: Exception) {
+
+            Result.failure()
+
+        }
+
+    }
+
+    private fun doMyJob(): String {
+
+        val savedImage = inputData.getString("editImage")
+
+        Thread.sleep(5000)
+
+        if (savedImage != null) {
+            Log.v(
+                "chainWorkers",
+                "Saved Image..."
+            )
+        }
+        return "image Saved"
+    }
+
+
+}
