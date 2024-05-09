a643bfe437b5b4e7ebbff6f5bf1b3f6ea6542730
Aliza <alizashahsavari@gmail.com>
2024-05-09 14:44:43 +0330

    Implement a SampleOneTimeWorker

--- app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
+++ app/src/main/java/com/aliza/alizaandroid/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.aliza.alizaandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.aliza.alizaandroid.base.BaseActivity
 import com.aliza.alizaandroid.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun inflateBinding() = ActivityMainBinding.inflate(layoutInflater)
 
+    private lateinit var workManager: WorkManager
+    private lateinit var sampleOneTimeWorker: OneTimeWorkRequest
+
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 1. create work manager =>
+        workManager = WorkManager.getInstance(this)
+
+        // 2. create our work =>
+        sampleOneTimeWorker()
+
+        // 3. work manager should do works =>
+        workManager.enqueue(sampleOneTimeWorker)
+
+    }
+
+    private fun sampleOneTimeWorker() {
+        sampleOneTimeWorker = OneTimeWorkRequest.from(SampleOneTimeWorker::class.java)
     }
 }
