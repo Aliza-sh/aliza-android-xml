afede69b12f32002ff21ff3ebf7db74d2f711ca6
Aliza <alizashahsavari@gmail.com>
2024-05-18 23:49:23 +0330

    Creating a koin for injecting viewModels 1. Create MainViewModel   1. Add MainViewModel in the appModule file 2. Injection in activity

--- app/src/main/java/com/aliza/alizaandroid/di/appModule.kt
+++ app/src/main/java/com/aliza/alizaandroid/di/appModule.kt
@@ -3,7 +3,9 @@ package com.aliza.alizaandroid.di
 import com.aliza.alizaandroid.di.module.NetModule
 import com.aliza.alizaandroid.di.module.imageModule.GlideLoader
 import com.aliza.alizaandroid.di.module.imageModule.ImageLoaderService
+import com.aliza.alizaandroid.ui.MainViewModel
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -11,5 +13,7 @@ val appModule = module {
     single <ImageLoaderService>{ GlideLoader(androidContext()) }
 
     single { NetModule().createApiService() }
+
+    viewModel{ MainViewModel(get()) }
 
 }
--- app/src/main/java/com/aliza/alizaandroid/ui/MainActivity.kt
+++ app/src/main/java/com/aliza/alizaandroid/ui/MainActivity.kt
@@ -2,25 +2,20 @@ package com.aliza.alizaandroid.ui
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.annotation.RequiresApi
 import com.aliza.alizaandroid.base.BaseActivity
 import com.aliza.alizaandroid.databinding.ActivityMainBinding
 import com.aliza.alizaandroid.di.module.imageModule.ImageLoaderService
-import com.aliza.alizaandroid.net.ApiService
-import com.aliza.alizaandroid.net.Student
 import com.aliza.alizaandroid.utils.URL_IMAGE
 import org.koin.android.ext.android.inject
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun inflateBinding() = ActivityMainBinding.inflate(layoutInflater)
 
     private val imageLoaderService : ImageLoaderService by inject()
 
-    private val apiService : ApiService by inject()
+    private val viewModel : MainViewModel by viewModel()
 
     @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,31 +23,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         
         imageLoaderService.load(URL_IMAGE, binding.img)
 
-        api()
-    }
-
-
-    private fun api() {
-        apiService.getAllStudents().enqueue(object : Callback<List<Student>> {
-            override fun onResponse(call: Call<List<Student>>, response: Response<List<Student>>) {
-                if (response.isSuccessful) {
-                    val data = response.body()
-                    if (data != null) {
-                        binding.txt.text = data.toString()
-                    } else {
-                        // Handle api null
-                        binding.txt.text = "data is null"
-                    }
-                } else {
-                    // Handle api error
-                    Log.v("errorApi", response.code().toString())
-                }
-            }
-
-            override fun onFailure(call: Call<List<Student>>, t: Throwable) {
-                Log.v("errorApi", t.message!!)
-            }
-
-        })
-    }
+        viewModel.getStudent()
+        viewModel.student.observe(this) {
+            binding.txt.text = it.toString()
+        }    }
 }
