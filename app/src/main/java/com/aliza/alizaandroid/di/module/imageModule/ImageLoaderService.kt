d583ece3986203fe538f3545977bf25582f9be24
Aliza <alizashahsavari@gmail.com>
2024-05-19 21:25:12 +0330

    CollectLatest , Collect , CollectIndexed

--- app/src/main/java/com/aliza/alizaandroid/di/module/imageModule/ImageLoaderService.kt
+++
@@ -1,7 +0,0 @@
-package com.aliza.alizaandroid.di.module.imageModule
-
-import android.widget.ImageView
-
-interface ImageLoaderService {
-    fun load(url: String, imageView: ImageView)
-}
--- app/src/main/java/com/aliza/alizaandroid/net/Student.kt
+++
@@ -1,7 +0,0 @@
-package com.aliza.alizaandroid.net
-
-data class Student(
-    val name: String,
-    val course: String,
-    val score: Int
-)
