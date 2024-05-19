ebd785cd9d7e7f624e829b2499887393b42ccd36
Aliza <alizashahsavari@gmail.com>
2024-05-19 19:14:18 +0330

    A simple example of using Flow

--- app/src/main/java/com/aliza/alizaandroid/ui/MainActivity.kt
+++ app/src/main/java/com/aliza/alizaandroid/ui/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.aliza.alizaandroid.ui
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.lifecycleScope
 import com.aliza.alizaandroid.base.BaseActivity
 import com.aliza.alizaandroid.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun inflateBinding() = ActivityMainBinding.inflate(layoutInflater)
-
+    private val viewModel = MainViewModel()
+    @RequiresApi(Build.VERSION_CODES.UPSIDE_DOWN_CAKE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launch {
+            viewModel.createFlow.collect {
+                Log.v("testFlow" , it.toString())
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/aliza/alizaandroid/ui/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.aliza.alizaandroid.ui
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class MainViewModel {
+    val createFlow : Flow<Int> = flow {
+        while (true) {
+            emit(  (1000..10000).random()  )
+            delay(100)
+        }
+    }
+}
