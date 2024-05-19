0d333d9cd9f425e32c8e224cfd00dda9ac30783f
Aliza <alizashahsavari@gmail.com>
2024-05-19 21:18:11 +0330

    CollectLatest , Collect , CollectIndexed

---
+++ app/src/main/java/com/aliza/alizaandroid/TestFlow.kt
@@ -0,0 +1,29 @@
+package com.aliza.alizaandroid
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectIndexed
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val flow = flowOf(1, 2, 3, 4, 5)
+
+    // collect
+    flow.collect { value ->
+        println("Collect: $value")
+        delay(1000)
+    }
+
+    // collectLatest
+    flow.collectLatest { value ->
+        println("CollectLatest: $value")
+        delay(1000)
+    }
+
+    // collectIndexed
+    flow.collectIndexed { index, value ->
+        println("CollectIndexed - Index: $index, Value: $value")
+    }
+
+}
